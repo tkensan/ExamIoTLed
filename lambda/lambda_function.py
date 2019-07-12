349690913eb2054db717050961f392735f51c368
tkensan <32670510+tkensan@users.noreply.github.com>
2019-07-12 12:06:46 +0900

    Add lambda function

---
+++ lambda/lambda_function.py
@@ -0,0 +1,44 @@
+import json
+
+import boto3
+
+
+shadow_update_data = {'state': {'reported': {'heartbeat': 0}}}
+
+def shadow_get_decode(dct):
+    n = dct['timestamp']
+    l = dct['metadata']['reported']['light']['timestamp']
+    return (n, l)
+
+def shadow_get_encode(now, last):
+    p = {
+        'timestamp': now,
+        'metadata': {'reported': {'light': {'timestamp': last}}} }
+    return p
+
+def payload_get(response):
+    b = response['payload']
+    p = json.loads(b.read())
+    print('payload get: ', p)
+    return p
+
+def payload_put(dct):
+    return json.dumps(dct)
+
+def lambda_handler(event, context):
+    name = event['thingname']
+    limit = event['limit']
+    print('thing: {}, limit: {}'.format(name, limit))
+    iot = boto3.client('iot-data')
+    r = iot.get_thing_shadow(thingName=name)
+    p = payload_get(r)
+    now, last = shadow_get_decode(p)
+    assert now >= last
+    delta = now - last
+    print('delta: ', delta)
+    if delta > limit:
+        print('Health check failed!')
+        iot.update_thing_shadow(
+            thingName=name, payload=payload_put(shadow_update_data))
+    return delta
+
