2815d11ab63360fcd9469912ba444ac23910fcac
tkensan <32670510+tkensan@users.noreply.github.com>
2019-07-12 18:15:50 +0900

    Add lambda test for thing name

--- lambda/test_lambda_function.py
+++ lambda/test_lambda_function.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import patch
 
+from botocore.exceptions import ParamValidationError
+
 import lambda_function as lf
 
 @patch('lambda_function.boto3')
@@ -8,6 +10,7 @@ class TestLambdaHandler(unittest.TestCase):
 
     thingname = 'testthing'
     lambdaevent = {'thingname': thingname, 'limit': 0}
+    lambdaevent_nokey = {'limit': 0}
     lambdaparam = {'event': lambdaevent, 'context': None}
 
     def config_payload(self, now, last):
@@ -17,6 +20,13 @@ class TestLambdaHandler(unittest.TestCase):
             '.__getitem__.return_value'
             '.read.return_value': lf.payload_put(lf.shadow_get_encode(
                 now, last)),
+            }
+        return c
+
+    def config_shadowget(self, effect):
+        c = {
+            'client.return_value'
+            '.get_thing_shadow.side_effect': effect
             }
         return c
 
@@ -39,6 +49,38 @@ class TestLambdaHandler(unittest.TestCase):
         self.assertRaises(
             AssertionError,
             lf.lambda_handler, event=self.lambdaevent, context=None)
+        mock.client.return_value.update_thing_shadow.assert_not_called()
+
+    def test_thingname_nothing(self, mock):
+        """
+        Test non existing thing name specified.
+        """
+        # botocore.errorfactory.ResourceNotFoundException in AWS lambda,
+        # but use Exception for now
+        mock.configure_mock(**(self.config_shadowget(Exception)))
+        self.assertRaises(
+            Exception,
+            lf.lambda_handler, event=self.lambdaevent, context=None)
+        mock.client.return_value.update_thing_shadow.assert_not_called()
+
+    def test_thingname_nostr(self, mock):
+        """
+        Test non string thing name specified.
+        """
+        mock.configure_mock(**(self.config_shadowget(ParamValidationError(
+            report='UnitTest'))))
+        self.assertRaises(
+            ParamValidationError,
+            lf.lambda_handler, event=self.lambdaevent, context=None)
+        mock.client.return_value.update_thing_shadow.assert_not_called()
+
+    def test_thingname_nokey(self, mock):
+        """
+        Test no thing name key available.
+        """
+        self.assertRaises(
+            KeyError,
+            lf.lambda_handler, event=self.lambdaevent_nokey, context=None)
         mock.client.return_value.update_thing_shadow.assert_not_called()
 
 if __name__ == '__main__':
